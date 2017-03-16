9fd6546b9b9e480db8cc370689526e080171d2ae
Kiss György <kissgyorgy@me.com>
2017-03-16 21:25:05 +0100

    Add url.py from urllib3.

--- certmaestro/exceptions.py
+++ certmaestro/exceptions.py
@@ -1,4 +1,8 @@
 class ConfigurationError(Exception):
+class CertmaestroError(Exception):
+    """Base Exception for all kinds of Certmaestro related errors."""
+
+
     """Something is wrong with Certmaestro's configuration.
     It's possible that we can't even find the backend because of this.
     """
@@ -18,3 +22,11 @@ class BackendError(Exception):
     """Something is wrong with the backend, the configuration might be good.
     e.g. Cannot connect.
     """
+
+
+class UrlParseError(ValueError, CertmaestroError):
+    "Raised when parse_url or similar fails to parse the URL input."
+
+    def __init__(self, location):
+        super().__init__("Failed to parse: %s" % location)
+        self.location = location
