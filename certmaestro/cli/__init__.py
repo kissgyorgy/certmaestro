6f0a711e80364495a6946127b448c2dd5eec043f
Kiss György <kissgyorgy@me.com>
2017-03-20 21:58:04 +0100

    Refact CLI setup_backend command

--- certmaestro/cli/__init__.py
+++ certmaestro/cli/__init__.py
@@ -72,11 +72,22 @@ def main(ctx, config_path, show_version):
 def setup_backend(ctx):
     """Initializes backend storage, settings roles, and generate CA."""
     config_path = get_config_path(ctx)
+    _check_config_path(config_path)
+    BackendCls = _select_backend()
+    builder = _ask_backend_params(BackendCls)
+    _make_new_config(builder, config_path)
+    click.echo(f'Saved configuration to {config_path}')
+    click.echo(f'Successfully initialized {backend.name}. You can issue certificates now!')
+
+
+def _check_config_path(config_path):
     if config_path.exists():
         click.confirm(f'Configuration file already exists: {config_path}\n'
                       'Do you want to replace it?', abort=True)
         click.echo()
 
+
+def _select_backend():
     click.echo('Backend choices:')
     for choice, Backend in enumerate(BACKENDS):
         click.echo(f'{choice + 1}. {Backend.name} - {Backend.description}')
@@ -85,6 +96,10 @@ def setup_backend(ctx):
                                   default=1)
     click.echo()
     BackendCls = BACKENDS[backend_choice - 1]
+    return BackendCls
+
+
+def _ask_backend_params(BackendCls):
     builder = BackendBuilder(BackendCls)
 
     while True:
@@ -97,14 +112,16 @@ def setup_backend(ctx):
         except ValueError as exc:
             click.echo(f'\nSomething is wrong with the configuration:\n  * {exc}')
 
+    return builder
+
+
+def _make_new_config(builder, config_path):
     backend = builder.setup_backend()
     config = Config.make_new(config_path)
     config.backend_name = backend.name
     str_values = {k: str(v) for k, v in builder.init_params.items()}
     config.backend_config.update(str_values)
     config.save()
-    click.echo(f'Saved configuration to {config_path}')
-    click.echo(f'Successfully initialized {backend.name}. You can issue certificates now!')
 
 
 @main.command('show-config')
