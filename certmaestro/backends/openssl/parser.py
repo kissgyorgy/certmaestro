36f7068414ecde6f266b53685f982220b5586e2e
Kiss György <kissgyorgy@me.com>
2017-03-26 10:05:29 +0200

    OpenSSL: database (index.txt) parser and tests

--- certmaestro/backends/openssl/parser.py
+++ certmaestro/backends/openssl/parser.py
@@ -1,8 +1,13 @@
 import re
+import mmap
+from pathlib import Path
+import attr
 from configparser import (
     Interpolation, InterpolationSyntaxError, InterpolationMissingOptionError,
     ConfigParser
 )
+from ...wrapper import FromFileMixin, SerialNumber
+
 
 class OpenSSLInterpolation(Interpolation):
     """Interpolation that is able to handle OpenSSL's special $dir values."""
@@ -41,3 +46,64 @@ class OpenSSLConfigParser(ConfigParser):
     def __init__(self, *args, env=None, **kwargs):
         self.env = env
         super().__init__(*args, **kwargs)
+
+
+@attr.s
+class OpenSSLDbEntry:
+    status = attr.ib()
+    expiration = attr.ib()
+    revocation = attr.ib()
+    serial_number = attr.ib()
+    filename = attr.ib()
+    dist_name = attr.ib()
+
+
+class OpenSSLDbParser(FromFileMixin):
+    """Parses OpenSSL's index.txt certificate database.
+    Every line in the file contains information about certificates. The format is the following:
+    1. Status flag (V=valid, R=revoked, E=expired).
+    2. Expiration date in YYMMDDHHMMSSZ format.
+    3. Revocation date in YYMMDDHHMMSSZ[,reason] format. Empty if not revoked.
+    4. Serial number in hex.
+    5. Filename or literal string ‘unknown’.
+    6. Distinguished name.
+    from: http://pki-tutorial.readthedocs.io/en/latest/cadb.html
+    """
+
+    def __init__(self, db_file: Path):
+        self._file = db_file
+        if not self._is_db_empty():
+            # have to keep the reference to the opened file
+            self._open_file = db_file.open('r+b')
+            self._mm = mmap.mmap(self._open_file.fileno(), 0)
+        else:
+            self._open_file = None
+            self._mm = None
+
+    def _is_db_empty(self):
+        return self._file.stat().st_size == 0
+
+    def __iter__(self):
+        # it can have content since we last tried
+        if self._is_db_empty():
+            return iter(())
+        self._mm.seek(0)
+        return self._iter_file()
+
+    def __del__(self):
+        if self._open_file is not None:
+            self._open_file.close()
+            self._mm.close()
+
+    def _iter_file(self):
+        for line in iter(self._mm.readline, b''):
+            columns = line.rstrip().decode().split('\t')
+            status, expiration, revocation, serial, filename, dist_name = columns
+            serial_number = SerialNumber(serial)
+            yield OpenSSLDbEntry(status, expiration, revocation, serial_number, filename,
+                                 dist_name)
+
+    def get_by_serial_number(self, serial_str: str):
+        for entry in self:
+            if entry.serial_number == SerialNumber(serial_str):
+                return entry
---
+++ tests/openssl/test_parser.py
@@ -0,0 +1,30 @@
+import pytest
+from pathlib import Path
+from certmaestro.backends.openssl.parser import OpenSSLDbParser
+
+
+@pytest.fixture(scope='session')
+def data_dir():
+    return Path(__file__).parent / 'data'
+
+
+@pytest.fixture(scope='session')
+def empty_file(data_dir):
+    return data_dir / 'empty.txt'
+
+
+class TestDatabaseParser:
+    def test_empty_database_is_fine(self, empty_file):
+        OpenSSLDbParser(empty_file)
+
+    def test_iterating_over_empty_file_gives_no_error(self, empty_file):
+        for entry in OpenSSLDbParser(empty_file):
+            pass
+
+    def test_get_by_serial_number_on_empty_file(self, empty_file):
+        db = OpenSSLDbParser(empty_file)
+        assert db.get_by_serial_number('01') is None
+
+    def test_get_by_serial_on_single_entry(self, data_dir):
+        db = OpenSSLDbParser(data_dir / 'one_valid.txt')
+        assert db.get_by_serial_number('01').dist_name == '/C=HU/L=Budapest/O=asf'
